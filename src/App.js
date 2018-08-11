e3ce81430b07608943b71946f61acd100afcd638
Tejasvi Thota <tejasvi.thota@transformingsystems.com>
2018-08-11 08:10:47 +0100

    Modifications to the way, how we fetch the information.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,62 @@ import externalLinks from "./content/external.json";
 import data from "./content/data.json";
 import "./App.css";
 
+const externalLinksURL =
+  "https://raw.githubusercontent.com/TejaBeta/tejabeta-2.0/master/src/content/external.json";
+const dataLinkURL =
+  "https://raw.githubusercontent.com/TejaBeta/tejabeta-2.0/master/src/content/data.json";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showCareer: false,
       showProjects: false,
-      showStack: false
+      showStack: false,
+      externalLinks: [],
+      currentOrganisation: null,
+
+      intro: null,
+      end: null,
+      projectInfo: [],
+      techStack: null,
+      careerInfo: null
     };
     this.handleOnClick = this.handleOnClick.bind(this);
+    this.getExternalLinks = this.getExternalLinks.bind(this);
+    this.getDataInfo = this.getDataInfo.bind(this);
+  }
+
+  componentDidMount() {
+    this.getExternalLinks();
+    this.getDataInfo();
+  }
+
+  getExternalLinks() {
+    fetch(externalLinksURL)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          externalLinks: data.data,
+          currentOrganisation: data.currentOrganisation
+        })
+      )
+      .catch(error => console.error("Something went wrong!"));
+  }
+
+  getDataInfo() {
+    fetch(dataLinkURL)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          intro: data.intro,
+          end: data.end,
+          projectInfo: data.projectInfo,
+          techStack: data.techStack,
+          careerInfo: data.careerInfo
+        });
+      })
+      .catch(error => console.error("Something went wrong!"));
   }
 
   handleOnClick(n) {
@@ -36,44 +83,52 @@ class App extends Component {
     }
   }
   render() {
-    const bioLinks = externalLinks.data.map(e => (
-      <p className="bio-links">
-        <a href={`${e.url}`} target="_blank">{`${e.title}`}</a>
-      </p>
-    ));
-    const technologies = data.techStack.languages.map(t => (
-      <li className="bullet-info">
-        <span className="bullet-highlights1">{`${t}`}</span>
-      </li>
-    ));
+    const bioLinks =
+      this.state.externalLinks &&
+      this.state.externalLinks.map(e => (
+        <p className="bio-links">
+          <a href={`${e.url}`} target="_blank">{`${e.title}`}</a>
+        </p>
+      ));
+    const technologies =
+      this.state.techStack &&
+      this.state.techStack.languages.map(t => (
+        <li className="bullet-info">
+          <span className="bullet-highlights1">{`${t}`}</span>
+        </li>
+      ));
 
-    const currentOrganisation = (
+    const currentOrganisation = this.state.currentOrganisation && (
       <a
         className="current-org"
-        href={`${externalLinks.currentOrganisation.url}`}
+        href={`${this.state.currentOrganisation.url}`}
         target="_blank"
-      >{`${externalLinks.currentOrganisation.title}`}</a>
+      >{`${this.state.currentOrganisation.title}`}</a>
     );
 
-    const careerInfo = data.careerInfo.map((c, i) => (
-      <li className="bullet-info">
-        <span className="bullet-highlights1">{`${c.time}`}</span>
-        <span className="bullet-highlights2">{`${c.company}`}</span>
-        <span className="bullet-highlights3">{`${c.title}`}</span>
-        <br />
-        <span className="bullet-description" hidden="true">{`${
-          c.Description
-        }`}</span>
-      </li>
-    ));
+    const careerInfo =
+      this.state.careerInfo &&
+      this.state.careerInfo.map((c, i) => (
+        <li className="bullet-info">
+          <span className="bullet-highlights1">{`${c.time}`}</span>
+          <span className="bullet-highlights2">{`${c.company}`}</span>
+          <span className="bullet-highlights3">{`${c.title}`}</span>
+          <br />
+          <span className="bullet-description" hidden="true">{`${
+            c.Description
+          }`}</span>
+        </li>
+      ));
 
-    const projectInfo = data.projectInfo.map(p => (
-      <li className="bullet-info">
-        <span className="bullet-highlights1">
-          <a href={`${p.url}`} target="_blank">{`${p.title}`}</a>
-        </span>
-      </li>
-    ));
+    const projectInfo =
+      this.state.projectInfo &&
+      this.state.projectInfo.map(p => (
+        <li className="bullet-info">
+          <span className="bullet-highlights1">
+            <a href={`${p.url}`} target="_blank">{`${p.title}`}</a>
+          </span>
+        </li>
+      ));
 
     return (
       <div className="App">
